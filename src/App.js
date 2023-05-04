987e18bd72193f17c955bebb322f4986616890fc
Steve De La Rosa <stevedelarosa1036@gmail.com>
2023-05-04 15:07:54 -0400

    updated btn

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
             <div className="flex items-center justify-center m-24">
                 <button
                     type="button"
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-md bg-indigo-600 px-6 py-2.5 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                     <a href="#">
                         Click Me
